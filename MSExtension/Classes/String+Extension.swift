6b0aee2ac6fc07e129b3848a5dc9ad1bab95297c
lifanfan <919016308@qq.com>
2019-09-11 15:56:43 +0800

    添加表情判断

--- MSExtension/Classes/String+Extension.swift
+++ MSExtension/Classes/String+Extension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public extension String {
-    // MARK - 手机号码判断
+    // MARK: - 手机号码判断
     var isPhoneNum: Bool {
         get{
             if self.count != 11 {
@@ -25,7 +25,7 @@ public extension String {
         }
     }
     
-    // MARK - 密码格式判断
+    // MARK: - 密码格式判断
     var isPassword: Bool {
         get {
             let mobileReg = "(^(?![0-9]+$)(?![a-zA-Z]+$)[a-zA-Z0-9]{6,18}$)"
@@ -37,6 +37,23 @@ public extension String {
             }
             return false
         }
+    }
+    // MARK: - 判断是否为表情
+    var characterCodingOut: Bool {
+        /* 遍历字符串的每一个字符 */
+        for str in self {
+            var byteCount = 0
+            /* 计算字符串的编码字节长度 */
+            /* UTF-8编码有可能是两个、三个、四个字节。Emoji表情是4个字节，而Mysql的utf8编码最多3个字节，所以数据插不进去。 */
+            for _ in String(str).utf8 {
+                byteCount = byteCount + 1
+            }
+            /* 大于等于四个字节的禁止输入 */
+            if byteCount >= 4 {
+                return true
+            }
+        }
+        return false
     }
     
 }
